78e52ec53e83ac9ae4f4050f0094e8ab6dabe8d1
zhangxianyi <502975825@qq.com>
2019-01-28 17:36:41 +0800

    first

--- src/main/java/fortest/demo.java
+++ src/main/java/fortest/demo.java
@@ -1,13 +1,34 @@
 package fortest;
 
 import org.apache.rocketmq.remoting.netty.NettyServerConfig;
+import rock.Connect;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class demo {
-    public static void main(String[] args) {
-//        NettyServerConfig nettyServerConfig = new NettyServerConfig();
-//        nettyServerConfig.setServerWorkerThreads(7);
-//        int serverWorkerThreads = nettyServerConfig.getServerWorkerThreads();
-//        System.out.println(serverWorkerThreads);
+    public static void main(String[] args) throws Exception{
+        Connect connect = new Connect();
+        Connection staticConnection = connect.getStaticConnection();
+        staticConnection.setAutoCommit(false);
+        try {
 
+
+            Statement statement = staticConnection.prepareStatement("a");
+            statement.executeUpdate("INSERT INTO agetest (age) VALUE (19)");
+
+            statement.executeUpdate("INSERT INTO agetest (age) VALUE ('g')");
+//        staticConnection.rollback();
+            staticConnection.commit();
+
+        }catch (Exception e){
+            staticConnection.rollback();
+            Statement statement = staticConnection.createStatement();
+            statement.executeUpdate("INSERT INTO agetest (age) VALUE (20)");
+            staticConnection.commit();
+            System.out.println("hi");
+        }
     }
 }
