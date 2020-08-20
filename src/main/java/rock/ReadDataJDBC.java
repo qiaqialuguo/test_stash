31c81ceb9f646a1299c8cf5c1041a9fb84feeb93
恰恰路过 <502975825@qq.com>
2020-08-20 23:45:27 +0800

    first

--- src/main/java/rock/ReadDataJDBC.java
+++ src/main/java/rock/ReadDataJDBC.java
@@ -40,7 +40,6 @@ public class ReadDataJDBC {
             everynum = Integer.parseInt(properties.getProperty("everynum"));
         }
         final String datatopic = p_dao.mq_topic;
-
         final String nameserver = properties.getProperty("nameserver");
         final String controltopic = properties.getProperty("controltopic");
         final String stopword = properties.getProperty("stopword");
