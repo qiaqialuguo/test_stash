cf8f26a153b0cf67a621743f14326ffa6cc985d5
恰恰路过 <502975825@qq.com>
2020-08-20 23:27:00 +0800

    first

--- src/main/java/fortest/demo.java
+++ src/main/java/fortest/demo.java
@@ -1,34 +1,7 @@
 package fortest;
 
-import org.apache.rocketmq.remoting.netty.NettyServerConfig;
-import rock.Connect;
-
-import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 public class demo {
     public static void main(String[] args) throws Exception{
-        Connect connect = new Connect();
-        Connection staticConnection = connect.getStaticConnection();
-        staticConnection.setAutoCommit(false);
-        try {
 
-
-            Statement statement = staticConnection.prepareStatement("a");
-            statement.executeUpdate("INSERT INTO agetest (age) VALUE (19)");
-
-            statement.executeUpdate("INSERT INTO agetest (age) VALUE ('g')");
-//        staticConnection.rollback();
-            staticConnection.commit();
-
-        }catch (Exception e){
-            staticConnection.rollback();
-            Statement statement = staticConnection.createStatement();
-            statement.executeUpdate("INSERT INTO agetest (age) VALUE (20)");
-            staticConnection.commit();
-            System.out.println("hi");
-        }
     }
 }
